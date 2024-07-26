94e9fb32ef9b1a584d4015c97adb73111086fdda
celiy <sasbrazilian@gmail.com>
2024-07-26 16:39:03 -0300

    Versão beta

--- appinfo.py
+++ appinfo.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 class SetAppinfo:
     def __init__(self):
-        self.versaoatual = "Build Beta"
-        self.dataversao = "20/07/2024"
+        self.versaoatual = "Build 1.0"
+        self.dataversao = "26/07/2024"
         self.criador = "Diogo Carvalho Viegas"
 
 def getappinfo():
@@ -14,10 +14,10 @@ def getappinfo():
     janelaso.resizable(False, False)
 
     Label(janelaso, text="Versão:").grid(row=0, column=0, padx=10, pady=10, sticky='w')
-    Label(janelaso, text="Build Beta").grid(row=0, column=1, padx=10, pady=10, sticky='e')
+    Label(janelaso, text="Build 1.0").grid(row=0, column=1, padx=10, pady=10, sticky='e')
 
     Label(janelaso, text="Data da Versão:").grid(row=1, column=0, padx=10, pady=10, sticky='w')
-    Label(janelaso, text="20/07/2024").grid(row=1, column=1, padx=10, pady=10, sticky='e')
+    Label(janelaso, text="26/07/2024").grid(row=1, column=1, padx=10, pady=10, sticky='e')
 
     Label(janelaso, text="Criador:").grid(row=2, column=0, padx=10, pady=10, sticky='w')
     Label(janelaso, text="Diogo Carvalho Viegas").grid(row=2, column=1, padx=10, pady=10, sticky='e')
